fe131ff1facd1e781791dacbdb936b90084ff647
narindergrintech <140406478+narindergrintech@users.noreply.github.com>
2023-08-08 10:28:29 +0530

    Update code.php

--- code.php
+++ code.php
@@ -1,1 +1,7 @@
-
+<div class="item_show_lowest_shipping">
+<?php echo $post->ID; ?>
+איסוף עצמי: <?php echo get_post_meta(get_the_ID(), 'sectionOneFormTitle', true)?>
+</div>
+<div class="item_delivery_time">
+זמן אספקה: <?php echo get_post_meta(get_the_ID(), 'sectionOneTitle', true)?>
+</div>
